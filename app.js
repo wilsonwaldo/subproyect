0d5f8fd2173e85d98b4732e83e3b410bf214db19
php.desteco@gmail.com <php.desteco@gmail.com>
2014-06-18 15:32:45 -0400

    Version 0.0.4

--- app.js
+++ app.js
@@ -35,6 +35,23 @@ app.use(function(req, res, next) {
 
 /// error handlers
 
+
+// Access-Control
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
+app.get('/', function(req, res, next) {
+  
+});
+
+app.post('/', function(req, res, next) {
+
+});
+// Access-Control
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
--- routes/calls.js
+++ routes/calls.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+/*router.all('*', function(req, res) {
+  res.render('llamadas', { titulo: 'Expressionality'});
+});*/
+
 /* GET llamadas listing. */
 router.get('/', function(req, res) {
   res.render('llamadas', { titulo: 'Expressionality'});
